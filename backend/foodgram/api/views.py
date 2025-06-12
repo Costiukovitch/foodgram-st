255e138ec46ef6a394109ee1eadcdf043ae5a930
Vlad Kostyukovich <mister.costiukovitch@yandex.ru>
2025-06-12 11:36:52 +0500

    update views.py

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,236 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import (
+    AllowAny, IsAuthenticatedOrReadOnly, IsAuthenticated
+)
 
-# Create your views here.
+from recipes.models import (
+    Ingredient, Tag, Recipe, Favorite, ShoppingCart, Follow,
+    IngredientInRecipe,
+)
+from users.models import User
+from .pagination import CustomPagination
+from .permissions import IsAuthorOrReadOnly
+from .filters import IngredientFilter, RecipeFilter
+from .serializers import (
+    RecipeSerializer, TagSerializer,
+    IngredientSerializer, FollowSerializer,
+    CustomUserSerializer, CreateRecipeSerializer,
+    AddFavoritesSerializer
+)
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """Ингредиенты."""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (AllowAny,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = IngredientFilter
+    search_fields = ('^name',)
+
+
+class RecipeViewSet(ModelViewSet):
+    """Рецепты."""
+
+    queryset = Recipe.objects.all()
+    pagination_class = CustomPagination
+    permission_classes = (IsAuthorOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = RecipeFilter
+
+    def get_serializer_class(self):
+        """Вызов нужного сериализатора."""
+
+        if self.action in ('list', 'retrieve'):
+            return RecipeSerializer
+        elif self.action in ('create', 'partial_update'):
+            return CreateRecipeSerializer
+
+    def get_serializer_context(self):
+        """Передача контекста."""
+
+        context = super().get_serializer_context()
+        context.update({'request': self.request})
+        return context
+
+    @action(
+        detail=False,
+        methods=('get',),
+        permission_classes=(IsAuthenticated,),
+        url_path='download_shopping_cart',
+        url_name='download_shopping_cart',
+    )
+    def download_shopping_cart(self, request):
+        """Загрузка ингредиентов для рецептов"""
+
+        ingredients = IngredientInRecipe.objects.filter(
+            recipe__shopping_recipe__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(sum=Sum('amount'))
+        shopping_list = self.ingredients_to_txt(ingredients)
+        return HttpResponse(shopping_list, content_type='text/plain')
+        
+    @action(
+        detail=True,
+        methods=('post', 'delete'),
+        permission_classes=(IsAuthenticated,),
+        url_path='favorite',
+        url_name='favorite',
+    )
+    def favorite(self, request, pk):
+        """Управление избранным."""
+
+        user = request.user
+        recipe = get_object_or_404(Recipe, id=pk)
+        if request.method == 'POST':
+            if Favorite.objects.filter(user=user, recipe=recipe).exists():
+                return Response(
+                    {'errors': f'Повторно - \"{recipe.name}\" добавить нельзя,'
+                               f'он уже есть в избранном у пользователя'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            Favorite.objects.create(user=user, recipe=recipe)
+            serializer = AddFavoritesSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':
+            obj = Favorite.objects.filter(user=user, recipe=recipe)
+            if obj.exists():
+                obj.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(
+                {'errors': f'В избранном нет рецепта \"{recipe.name}\"'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    @action(
+        detail=True,
+        methods=('post', 'delete'),
+        permission_classes=(IsAuthenticated,),
+        url_path='shopping_cart',
+        url_name='shopping_cart',
+    )
+    def shopping_cart(self, request, pk):
+        """Управление списком покупок."""
+
+        user = request.user
+        recipe = get_object_or_404(Recipe, id=pk)
+
+        if request.method == 'POST':
+            if ShoppingCart.objects.filter(user=user, recipe=recipe).exists():
+                return Response(
+                    {'errors': f'Повторно - \"{recipe.name}\" добавить нельзя,'
+                               f'он уже есть в списке покупок'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            ShoppingCart.objects.create(user=user, recipe=recipe)
+            serializer = AddFavoritesSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':
+            obj = ShoppingCart.objects.filter(user=user, recipe__id=pk)
+            if obj.exists():
+                obj.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(
+                {'errors': f'Нельзя удалить рецепт - \"{recipe.name}\", '
+                           f'которого нет в списке покупок '},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    @staticmethod
+    def ingredients_to_txt(ingredients):
+        """Объединение ингредиентов в список."""
+
+        shopping_list = ''
+        for ingredient in ingredients:
+            shopping_list += (
+                f"{ingredient['ingredient__name']}  - "
+                f"{ingredient['sum']}"
+                f"({ingredient['ingredient__measurement_unit']})\n"
+            )
+        return shopping_list
+        
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Теги к рецепту."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AllowAny,)
+
+
+class CustomUserViewSet(UserViewSet):
+    """Пользователи и подписки."""
+
+    queryset = User.objects.all()
+    pagination_class = LimitOffsetPagination
+    serializer_class = CustomUserSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    @action(
+        detail=False,
+        methods=('get',),
+        permission_classes=(IsAuthenticated, ),
+        url_path='subscriptions',
+        url_name='subscriptions',
+    )
+    def subscriptions(self, request):
+        """Создание страницы подписок."""
+
+        queryset = User.objects.filter(follow__user=self.request.user)
+        if queryset:
+            pages = self.paginate_queryset(queryset)
+            serializer = FollowSerializer(pages, many=True,
+                                          context={'request': request})
+            return self.get_paginated_response(serializer.data)
+        return Response('Вы ни на кого не подписаны.',
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    @action(
+        detail=True,
+        url_path='subscribe',
+        url_name='subscribe',
+        methods=('post', 'delete'),
+        permission_classes=(IsAuthenticated,),
+    )
+    def subscribe(self, request, id):
+        """Управление подписками."""
+
+        user = request.user
+        author = get_object_or_404(User, id=id)
+        change_subscription_status = Follow.objects.filter(
+            user=user.id, author=author.id
+        )
+        if request.method == 'POST':
+            if user == author:
+                return Response('Вы пытаетесь подписаться на себя!',
+                                status=status.HTTP_400_BAD_REQUEST)
+            if change_subscription_status.exists():
+                return Response(f'Вы теперь подписаны на {author}',
+                                status=status.HTTP_400_BAD_REQUEST)
+            subscribe = Follow.objects.create(
+                user=user,
+                author=author
+            )
+            subscribe.save()
+            return Response(f'Вы подписались на {author}',
+                            status=status.HTTP_201_CREATED)
+        if change_subscription_status.exists():
+            change_subscription_status.delete()
+            return Response(f'Вы отписались от {author}',
+                            status=status.HTTP_204_NO_CONTENT)
+        return Response(f'Вы не подписаны на {author}',
+                        status=status.HTTP_400_BAD_REQUEST)
